b99ff2a0674e198ec9a7e8a0c9fdebd8a89263fa
yokit9 <yoko_914215_yoko@hotmail.com>
2019-05-04 20:27:30 +0900

    agc029b(notAC)

--- agc/agc029/b/Main.java
+++ agc/agc029/b/Main.java
@@ -3,23 +3,78 @@ package b;
 import static java.lang.System.*;
 import java.util.*;
 
+class Num {
+	int num;
+	int count;
+	
+	Num(int _num, int _count) {
+		num = _num;
+		count = _count;
+	}
+	
+	public String toString() {
+		return num + " " + count;
+	}
+}
+
 public class Main {
+	public static int getMSB(int n) {
+		n = (n & 0xffff0000) > 0 ? n & 0xffff0000 : n;
+		n = (n & 0xff00ff00) > 0 ? n & 0xff00ff00 : n;
+		n = (n & 0xf0f0f0f0) > 0 ? n & 0xf0f0f0f0 : n;
+		n = (n & 0xcccccccc) > 0 ? n & 0xcccccccc : n;
+		n = (n & 0xaaaaaaaa) > 0 ? n & 0xaaaaaaaa : n;
+		
+		return n;
+	}
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(in);
 
-		HashSet<Long> two = new HashSet<Long>();
-		for(long i=0; i*i<=2000000000; i++) two.add(i*i);
-		
         int n = sc.nextInt();
-		long[] num = new long[n];
-		for(int i=0; i<n; i++) num[i] = sc.nextLong();
+		int[] a = new int[n];
+		for(int i=0; i<n; i++) a[i] = sc.nextInt();
+		Arrays.sort(a);
 		
-		// 2 2 7 14
-		// 1 4 9 16 25 36 49 64 81 100
-		//  3 5 7  9  11 13 15 17 19
+		ArrayList<Num> nums = new ArrayList<Num>();
+		nums.add(new Num(a[n-1], 0));
+		for(int i=n-1; i>=0; i--) {
+			Num last = nums.get(nums.size()-1);
+			if(last.num == a[i]) 
+				last.count = last.count+1;
+			else 
+				nums.add(new Num(a[i], 1));
+		}
+		//out.println(nums);
 		
+		int ans = 0;
+		for(int i = 0; i<nums.size(); i++) {
+			Num num = nums.get(i);
+			if(num.count <= 0) continue;
+			num.count--;
+			
+			int pair = (getMSB(num.num) << 1) - num.num;
+			
+			int p = i, q = nums.size()-1;
+			int mid = q;
+			while(p < q) {
+				Num mNum = nums.get(mid);
+				if(mNum.num == pair) {
+					if(mNum.count > 0) {
+						ans++;
+						mNum.count--;
+					}
+					break;
+				}
+				else if(mNum.num > pair) p = mid;
+				else q = mid;
+				mid = (p+q)/2;
+			}
+			//out.println("[" + num + "] " + mid + " " + nums + " " + ans);
+		}
 		
-        out.println();
+        out.println(ans);
     }
 }
+
 
